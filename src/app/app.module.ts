b2e768f76e057e8815c354e6ac901aaa78a7ceda
Wasif <wasif.naeem786@gmail.com>
2018-08-16 23:43:18 +0500

    1st build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { StopWatchComponent } from './components/stop-watch/stop-watch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    StopWatchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
