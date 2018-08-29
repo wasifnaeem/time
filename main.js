37616494b4b1c9a0a4288dd9ac96ddff1141d5ce
Wasif <wasif.naeem786@gmail.com>
2018-08-29 23:55:13 +0500

    changes made for packaging

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ let win
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 })
-
+  win.setMenu(null)
   // and load the index.html of the app.
   win.loadFile('./dist/time/index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -22,14 +22,6 @@ function createWindow() {
     win = null
   })
 }
-
-// This method will be calle
-win.on('closed', () => {
-  // Dereference the window object, usually you would store windows
-  // in an array if your app supports multi windows, this is the time
-  // when you should delete the corresponding element.
-  win = null
-})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
