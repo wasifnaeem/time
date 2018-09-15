cd64be3a50ff1fbec153790166ddce1032930129
Wasif <wasif.naeem786@gmail.com>
2018-09-15 17:48:37 +0500

    some changes and improvements

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-// const electron, { app, BrowserWindow } = require('electron')
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 // start: Squirrel
 if (handleSquirrelEvent(app)) {
@@ -12,56 +11,54 @@ if (handleSquirrelEvent(app)) {
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow() {
-
   // Create the browser window.
-  win = new BrowserWindow({ show: false })
-  win.maximize()
+  win = new BrowserWindow({ show: false });
+  win.maximize();
 
-  win.setMenu(null)
+  win.setMenu(null);
   // and load the index.html of the app.
-  win.loadFile('./dist/time/index.html')
+  win.loadFile("./dist/time/index.html");
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    win = null;
+  });
 
   win.once("ready-to-show", () => {
-    win.show()
-  })
-
+    win.show();
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
@@ -72,56 +69,56 @@ function handleSquirrelEvent(application) {
     return false;
   }
 
-  const ChildProcess = require('child_process');
-  const path = require('path');
+  const ChildProcess = require("child_process");
+  const path = require("path");
 
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const appFolder = path.resolve(process.execPath, "..");
+  const rootAtomFolder = path.resolve(appFolder, "..");
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, "Update.exe"));
   const exeName = path.basename(process.execPath);
 
-  const spawn = function (command, args) {
+  const spawn = function(command, args) {
     let spawnedProcess, error;
 
     try {
       spawnedProcess = ChildProcess.spawn(command, args, {
         detached: true
       });
-    } catch (error) { }
+    } catch (error) {}
 
     return spawnedProcess;
   };
 
-  const spawnUpdate = function (args) {
+  const spawnUpdate = function(args) {
     return spawn(updateDotExe, args);
   };
 
   const squirrelEvent = process.argv[1];
   switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
+    case "--squirrel-install":
+    case "--squirrel-updated":
       // Optionally do things such as:
       // - Add your .exe to the PATH
       // - Write to the registry for things like file associations and
       //   explorer context menus
 
       // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
+      spawnUpdate(["--createShortcut", exeName]);
 
       setTimeout(application.quit, 1000);
       return true;
 
-    case '--squirrel-uninstall':
+    case "--squirrel-uninstall":
       // Undo anything you did in the --squirrel-install and
       // --squirrel-updated handlers
 
       // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
+      spawnUpdate(["--removeShortcut", exeName]);
 
       setTimeout(application.quit, 1000);
       return true;
 
-    case '--squirrel-obsolete':
+    case "--squirrel-obsolete":
       // This is called on the outgoing version of your app before
       // we update to the new version - it's the opposite of
       // --squirrel-updated
@@ -129,5 +126,5 @@ function handleSquirrelEvent(application) {
       application.quit();
       return true;
   }
-};
-  // end: Squirrel
+}
+// end: Squirrel
--- src/app/components/timer-components/timer/timer.component.ts
+++ src/app/components/timer-components/timer/timer.component.ts
@@ -1,195 +1,202 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { TimerService } from '../../../services/timer.service';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { TimeupComponent } from '../timeup/timeup.component';
-import { ITimeUp } from '../../../interfaces/time-up.interface';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { TimerService } from "../../../services/timer.service";
+import { MatDialog, MatDialogRef } from "@angular/material";
+import { TimeupComponent } from "../timeup/timeup.component";
+import { ITimeUp } from "../../../interfaces/time-up.interface";
 
 @Component({
-  selector: 'app-timer',
-  templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  selector: "app-timer",
+  templateUrl: "./timer.component.html",
+  styleUrls: ["./timer.component.scss"]
 })
 export class TimerComponent implements OnInit, ITimeUp {
+  @ViewChild("media")
+  media: ElementRef;
+  isStarted: boolean = false;
+  isPaused: boolean = false;
 
-  @ViewChild('media') media: ElementRef
-  isStarted: boolean = false
-  isPaused: boolean = false
-
-  audioFile: string = './assets/audio/time_up.mp3'
-  repeatAlarm: boolean = true
+  is_small_screen: boolean = false;
+  audioFile: string = "./assets/audio/time_up.mp3";
+  repeatAlarm: boolean = true;
 
   innerHeight: any;
   innerWidth: any;
 
-  constructor(
-    private timerService: TimerService,
-    private dialog: MatDialog
-  ) {
-    this.innerHeight = window.screen.height / 6
-    this.innerWidth = window.screen.width
+  constructor(private timerService: TimerService, private dialog: MatDialog) {
+    this.innerHeight = window.screen.height / 6;
+    this.innerWidth = window.screen.width;
   }
 
   ngOnInit() {
-    this.isStarted = false
-    this.isPaused = false
-    this.Audio.volume = 1
+    this.isStarted = false;
+    this.isPaused = false;
+    this.Audio.volume = 1;
 
-    this.timerService.componentClassRef_Subject.next(this)
-    this.onTimeUp()
+    this.timerService.componentClassRef_Subject.next(this);
+    this.onTimeUp();
+  }
+
+  screenSizeChanged() {
+    console.log("size is being changed");
+
+    if (this.is_small_screen) {
+      // logic here
+    }
   }
 
   // start: Time Events
   onTimeUp() {
     this.timerService.isTimeUp_Subject.subscribe((isTimeUp: boolean) => {
       if (isTimeUp) {
-        this.playSound()
-        this.openOKdialog()
+        this.playSound();
+        this.openOKdialog();
       }
-    })
+    });
   }
 
   start() {
     if (this.timerService.start()) {
-      this.isStarted = true
-      this.isAudioPlaying = false
+      this.isStarted = true;
+      this.isAudioPlaying = false;
     }
   }
 
   pause() {
-    this.isPaused = true
-    this.timerService.pause()
+    this.isPaused = true;
+    this.timerService.pause();
   }
 
   continue() {
-    this.isPaused = false
-    this.timerService.start()
+    this.isPaused = false;
+    this.timerService.start();
   }
 
   stop() {
-    this.isStarted = false
-    this.isPaused = false
-    this.timerService.stop()
+    this.isStarted = false;
+    this.isPaused = false;
+    this.timerService.stop();
   }
   // end: Time Events
 
   // start: Settings Events
-  isSettingsOpened: boolean = false
-  isRotate: boolean = false
+  isSettingsOpened: boolean = false;
+  isRotate: boolean = false;
   openSettings() {
     if (this.isSettingsOpened) {
-      this.isSettingsOpened = false
-      return
+      this.isSettingsOpened = false;
+      return;
     }
 
-    this.isSettingsOpened = true
-    this.isRotate = !this.isRotate
+    this.isSettingsOpened = true;
+    this.isRotate = !this.isRotate;
     setTimeout(() => {
-      this.isRotate = !this.isRotate
+      this.isRotate = !this.isRotate;
     }, 500);
   }
   closeSettings() {
-    this.isSettingsOpened = false
+    this.isSettingsOpened = false;
   }
   // end: Settings Events
 
   // start: Sound Events
-  isAudioPlaying: boolean = false
+  isAudioPlaying: boolean = false;
   playSound() {
-    this.Audio.play()
-    this.isStarted = false
-    this.isAudioPlaying = true
+    this.Audio.play();
+    this.isStarted = false;
+    this.isAudioPlaying = true;
   }
   end() {
-    this.isAudioPlaying = false
+    this.isAudioPlaying = false;
   }
   // end: Sound Events
 
   // start: Events
   onHourFocusOut() {
     if (+this.Hours === 0 || this.Hours === undefined || +this.Hours === NaN)
-      this.Hours = '00'
-    else if (+this.Hours < 10)
-      this.Hours = '0' + +this.Hours
-    else if (+this.Hours > 23)
-      this.Hours = '23'
-    else
-      this.Hours = (+this.Hours).toString()
+      this.Hours = "00";
+    else if (+this.Hours < 10) this.Hours = "0" + +this.Hours;
+    else if (+this.Hours > 23) this.Hours = "23";
+    else this.Hours = (+this.Hours).toString();
   }
 
   onMinuteFocusOut() {
-    if (+this.Minutes === 0 || this.Minutes === undefined || +this.Minutes === NaN)
-      this.Minutes = '00'
-    else if (+this.Minutes < 10)
-      this.Minutes = '0' + +this.Minutes
-    else if (+this.Minutes > 59)
-      this.Minutes = '59'
-    else
-      this.Minutes = (+this.Minutes).toString()
+    if (
+      +this.Minutes === 0 ||
+      this.Minutes === undefined ||
+      +this.Minutes === NaN
+    )
+      this.Minutes = "00";
+    else if (+this.Minutes < 10) this.Minutes = "0" + +this.Minutes;
+    else if (+this.Minutes > 59) this.Minutes = "59";
+    else this.Minutes = (+this.Minutes).toString();
   }
 
   onSecondFocusOut() {
-    if (+this.Seconds === 0 || this.Seconds === undefined || +this.Seconds === NaN)
-      this.Seconds = '00'
-    else if (+this.Seconds < 10)
-      this.Seconds = '0' + +this.Seconds
-    else if (+this.Seconds > 59)
-      this.Seconds = '59'
-    else
-      this.Seconds = (+this.Seconds).toString()
+    if (
+      +this.Seconds === 0 ||
+      this.Seconds === undefined ||
+      +this.Seconds === NaN
+    )
+      this.Seconds = "00";
+    else if (+this.Seconds < 10) this.Seconds = "0" + +this.Seconds;
+    else if (+this.Seconds > 59) this.Seconds = "59";
+    else this.Seconds = (+this.Seconds).toString();
   }
 
   // end: Events
 
   // start: properties
   get Seconds(): string {
-    return this.timerService.Seconds
+    return this.timerService.Seconds;
   }
 
   set Seconds(val: string) {
-    this.timerService.Seconds = val
+    this.timerService.Seconds = val;
   }
 
   get Minutes(): string {
-    return this.timerService.Minutes
+    return this.timerService.Minutes;
   }
 
   set Minutes(val: string) {
-    this.timerService.Minutes = val
+    this.timerService.Minutes = val;
   }
 
   get Hours(): string {
-    return this.timerService.Hours
+    return this.timerService.Hours;
   }
 
   set Hours(val: string) {
-    this.timerService.Hours = val
+    this.timerService.Hours = val;
   }
 
   get Audio(): HTMLAudioElement {
-    return this.media.nativeElement
+    return this.media.nativeElement;
   }
 
   get Volume() {
-    return this.Audio.volume * 100
+    return this.Audio.volume * 100;
   }
   set Volume(val) {
-    this.Audio.volume = val / 100
+    this.Audio.volume = val / 100;
   }
   // end: properties
 
   // start: non-related
-  dialogRef: MatDialogRef<TimeupComponent>
+  dialogRef: MatDialogRef<TimeupComponent>;
   closeDialog_ITimeUp() {
-    this.Audio.pause()
-    this.Audio.currentTime = 0
-    this.dialogRef.close()
+    this.Audio.pause();
+    this.Audio.currentTime = 0;
+    this.dialogRef.close();
   }
 
   openOKdialog() {
-    this.dialogRef = this.dialog.open(TimeupComponent, { width: "50%", disableClose: true })
-    let component = this.dialogRef.componentInstance
-    component.timeup_parentRef = this
+    this.dialogRef = this.dialog.open(TimeupComponent, {
+      width: "50%",
+      disableClose: true
+    });
+    let component = this.dialogRef.componentInstance;
+    component.timeup_parentRef = this;
   }
   // end: non-related
-
 }
